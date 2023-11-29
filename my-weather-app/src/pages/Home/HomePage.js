fde16b5fe23038c3bb14b6775174efb97fe16247
HiruniSew <hiruni.kahawalage@innovative-e.com>
2023-11-29 11:49:42 +0530

    Added folder structures.

---
+++ my-weather-app/src/pages/Home/HomePage.js
@@ -0,0 +1,11 @@
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <h1>Hello World Hiruni </h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ my-weather-app/src/pages/Places/PlacesPage.js
@@ -0,0 +1,11 @@
+import "./PlacesPage.css";
+
+const PlacesPage = () => {
+  return (
+    <div className="App">
+      <h1>Hi hiru</h1>
+    </div>
+  );
+};
+
+export default PlacesPage;
